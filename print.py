ca1bcfeb77a5239e32adea33bd8a8bc28d47a63b
shreyasaliya <106687363+shreyasaliya@users.noreply.github.com>
2022-06-01 21:24:55 +0530

    Add files via upload

---
+++ print.py
@@ -0,0 +1,19 @@
+print("Hello World")
+
+#This is comment
+
+"""
+This
+is
+comments
+"""
+
+print(1+78)
+print("1*78")
+
+print("shreya is girl", end="=")
+print(20+10)
+
+print("shreya is \ngood girl \t1")
+
+print("shreya loves abhi","forever and ever")
