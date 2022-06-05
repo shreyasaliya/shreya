3c2af10aa5901a1c8718cbb67ec8fa964963ab82
shreyasaliya <106687363+shreyasaliya@users.noreply.github.com>
2022-06-05 20:57:26 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,29 @@
+var1 = "Hello"
+var4 = "89"
+var5 = "64"
+var2 = 4
+var3 = 36.7
+
+# print(var1+var4)
+# print(int(var4) + int(var5))
+# print(100 * str(int(var4) + int(var5)))
+# print(type(var3))
+# print(10 * "Hello World\n")
+
+
+# print("Enter your number:")
+# inpnum = int(input())
+# print("You entered ", inpnum+10)
+
+
+print("enter first num:")
+fn = input()
+print("enter second num:")
+sn = input()
+print("sum of these two numbers:", int(fn)+int(sn))
+
+"""
+str()
+int()
+float()
+"""
