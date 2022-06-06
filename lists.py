28f40780b788ce208575160dc3e65307a8026672
shreyasaliya <106687363+shreyasaliya@users.noreply.github.com>
2022-06-06 22:30:38 +0530

    Add files via upload

---
+++ lists.py
@@ -0,0 +1,70 @@
+from cgi import print_arguments
+from distutils.util import change_root
+
+
+grocery = ["Harpic", "vim bar", "Lollypop", "banana", "Bhindi", 56]
+# print(grocery)
+print(grocery[2])
+
+numbers = [2, 7, 9, 10, 3]
+# print(numbers)
+print(numbers[4])
+# sort and reverse change original number list
+# numbers.sort()
+# numbers.reverse()
+# print(numbers)
+
+# print(numbers[0:5])
+# print(numbers[:5])
+# print(numbers[:])
+# print(numbers[1:])
+# print(numbers[1:4])
+# print(numbers)
+
+# print(numbers[::1])
+# print(numbers[::2])
+# print(numbers[::3])
+# print(numbers[::-1])
+# print(numbers[1:5:2])
+# print(len(numbers))
+# print(max(numbers))
+# print(min(numbers))
+
+# numbers.append(71) #add num in last of list
+# numbers.append(43) #add num in last of list
+# print(numbers)
+
+# num = []
+# num.append(4)
+# num.append(67)
+# num.append(89)
+# print(num)
+
+# numbers.insert(1,67) #index and num you want to add
+# numbers.remove(9) #remove 9 from list
+# numbers.pop() #remove last element of list
+# print(numbers)
+
+
+# numbers[1] = 98
+# print(numbers)
+
+# Mutable - can change - list
+# Immutable - cannot change - tupple
+
+# tupple
+tp = (1,2,3)
+# tp1 = (1,)
+# tp[1] = 8
+print(tp)
+# print(tp1)
+
+
+a = 1
+b = 8
+a,b = b,a   #program
+# temp = a
+# a = b
+# b = temp
+print(a,b)
+
---
+++ string_slicing.py
@@ -0,0 +1,34 @@
+mystr = "shreya is good girl"
+
+# print(mystr[5])
+# print(mystr[0:6])
+# print(len(mystr))
+# print(mystr[0:19])
+# print(mystr[0:89])
+# print(mystr[0:6:2])
+
+# print(mystr[::])
+# print(mystr[:19])
+# print(mystr[::3])
+# print(mystr[::5559])
+
+# print(mystr[-1:0])
+# print(mystr[-4:])
+# print(mystr[-4:-2])
+# print(mystr[15:17])
+# print(mystr[::-1])
+# print(mystr[::-2])
+
+
+mystring = "goodmorning"
+print(mystring.isalnum())
+print(mystring.isalpha())
+print(mystring.endswith("ing"))
+print(mystring.count("o"))
+print(mystring.capitalize())
+print(mystring.find("dm"))
+print(mystring.upper())
+print(mystring.lower())
+print(mystring.replace("dm", "ddmm"))
+
+
